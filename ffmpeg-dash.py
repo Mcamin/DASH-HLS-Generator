b185aa5d51ff947c037cb2ff066d8424b2d34d9d
Ahmed Amine Mchayaa <ahmed.amine.mchayaa@fokus.fraunhofer.de>
2022-11-08 14:06:35 +0100

    Updated ffmpeg-dash

--- ffmpeg-dash.py
+++ ffmpeg-dash.py
@@ -35,60 +35,15 @@ def build_dash_command(cf):
     cmd = cb.add_fps(cmd, cf)
     cmd = cb.add_video_codec(cmd, cf)
     cmd = cb.add_pixel_format(cmd, cf)
-
-
-    f"""{ffmpeg }This is my quoted variable: "{var_a}". """
-    ## checkInpu
-
-    command_list = [
-        ffmpeg,
-        "-re -i",
-        cf["input_file"],
-        "-y -preset",
-        cf["preset"],
-        "-keyint_min",
-        cf["keyint_min"],
-        "-g",
-        cf["gop_size"],
-        "-sc_threshold",
-        cf["sc_threshold"],
-        "-r",
-        cf["fps"],
-        "-c:v ",
-        cf["codec_video"],
-        "-pix_fmt",
-        cf["pixel_format"],
-        "-c:a",
-        cf["codec_audio"],
-        "-b:a",
-        cf["bitrate_audio"],
-        "-ac",
-        cf["audio_channel"],
-        "-ac",
-        cf["audio_channel"],
-        "-ar",
-        cf["audio_rate"],
-        "-map v:0 -s:0 $V_SIZE -b:v:0 2000K -maxrate:0 2000K -bufsize:0 2000K/2",
-        "-map v:0 -s:1 $V_SIZE -b:v:1 1000K -maxrate:1 1000K -bufsize:1 1000K/2",
-        "-map v:0 -s:2 $V_SIZE -b:v:2 500K -maxrate:2 500K -bufsize:2 500K/2",
-        "-map 0:a",
-        "-init_seg_name",
-        cf["init_seg_name"],
-        "-media_seg_name",
-        cf["media_seg_name"],
-        "-use_template",
-        cf["use_template"],
-        "-use_timeline",
-        cf["use_timeline"],
-        "-seg_duration",
-        cf["segment_duration"],
-        "-adaptation_sets",
-        cf["adaptation_sets_fmt"],
-        "-f dash",
-        cf["mpd_filepath"],
-        "index.mpd"
-    ]
-    return command_list
+    cmd = cb.add_audio_codec(cmd, cf)
+    cmd = cb.add_audio_bitrate(cmd, cf)
+    cmd = cb.add_audio_channel(cmd, cf)
+    cmd = cb.add_audio_rate(cmd, cf)
+    cmd = cb.add_filters(cmd, cf)
+    cmd = cb.add_video_mapping(cmd, cf)
+    cmd = cb.add_audio_mapping(cmd, cf)
+    cmd = cb.add_dash_attributes(cmd, cf)
+    return cmd
 
 
 def create_directory(path):
@@ -100,10 +55,10 @@ def create_directory(path):
 def generate_dash_files(configs):
     for cf in configs:
         cmd = build_dash_command(cf)
-        #cmd = [ffmpeg, "-i", "./media/4k60fps.webm ","./media/output.mp4"]
         create_directory(cf["mpd_filepath"])
         print(cmd)
-        if subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT).returncode == 0:
+        # shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
+        if subprocess.run(cmd).returncode == 0:
             print("FFmpeg Script Ran Successfully")
         else:
             print("There was an error running your FFmpeg script")
